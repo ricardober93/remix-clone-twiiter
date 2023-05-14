47d5f6643f8c08a6a67bc40811f16c126c30b3b9
ricardo bermudez <ricardobzorro@gmail.com>
2023-05-14 10:05:56 -0500

    update package.json

---
+++ prisma/migrations/20230505032411_migration/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Note` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Note" DROP CONSTRAINT "Note_userId_fkey";
+
+-- DropTable
+DROP TABLE "Note";
+
+-- CreateTable
+CREATE TABLE "Twit" (
+    "id" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Twit_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Twit" ADD CONSTRAINT "Twit_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/migrations/20230512015403_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "backgroundUser" TEXT,
+ADD COLUMN     "descrptionUser" TEXT,
+ADD COLUMN     "photoUser" TEXT,
+ADD COLUMN     "username" TEXT;
